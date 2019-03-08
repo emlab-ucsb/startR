76a98b8fc44ba0e5f47439f6e11d58cb2766310f
jcvdav <juancarlos.villader@gmail.com>
2019-03-08 09:54:30 -0800

    Fixed my theme

--- R/ggtheme_plot.R
+++ R/ggtheme_plot.R
@@ -2,31 +2,58 @@
 #'
 #' @description Creates a standard theme for all my plots
 #'
-#' @param base_size
+#' @param font_size The size of the font for \code{element_text()}. Defaults to \code{font_size = 10}.
 #'
 #' @export
 #'
-ggtheme_plot <- function(base_size = 9) {
-  theme(axis.ticks = element_blank(),
-        text = element_text(
-          #family = "Helvetica",
-          color = "gray30",
-          size = base_size),
-        plot.title = element_text(
-          size = rel(1.25),
-          hjust = 0,
-          face = "bold"),
-        panel.background = element_blank(),
-        strip.background = element_rect(fill = "transparent"),
-        strip.text = element_text(
-          size = base_size,
-          face = "italic"),
-        legend.position = "right",
-        panel.border = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.grid.major = element_blank(),
-        legend.key = element_rect(
-          colour = NA,
-          fill = NA),
-        axis.line = element_blank()
-  )}
+ggtheme_plot <- function (font_size = 10, font_family = "", line_size = 0.5) {
+
+  half_line <- font_size / 2
+
+  theme_grey(base_size = font_size, base_family = font_family) %+replace%
+    theme(rect = element_rect(fill = "transparent",
+                              colour = NA,
+                              color = NA,
+                              size = 0,
+                              linetype = 0),
+          text = element_text(family = font_family,
+                              face = "plain",
+                              colour = "black",
+                              size = font_size,
+                              hjust = 0.5,
+                              vjust = 0.5,
+                              angle = 0,
+                              lineheight = 0.9,
+                              margin = margin(),
+                              debug = FALSE),
+          axis.text = element_text(colour = "black",
+                                   size = rel(0.8)),
+          axis.ticks = element_line(colour = "black"),
+          axis.line = element_line(colour = "black",
+                                   size = line_size,
+                                   lineend = "square"),
+          axis.line.x = element_line(colour = "black",
+                                     size = line_size,
+                                     lineend = "square"),
+          axis.line.y = element_line(colour = "black",
+                                     size = line_size,
+                                     lineend = "square"),
+          legend.background = element_blank(),
+          legend.key = element_blank(),
+          legend.key.size = unit(1, "lines"),
+          legend.spacing = unit(0.4, "cm"),
+          legend.text = element_text(size = rel(0.8)),
+          legend.justification = c("left", "center"),
+          panel.background = element_blank(),
+          panel.border = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          strip.text = element_text(size = rel(0.8)),
+          strip.background = element_blank(),
+          plot.background = element_blank(),
+          plot.title = element_text(face = "bold",
+                                    size = font_size,
+                                    margin = margin(b = half_line)),
+          complete = TRUE
+    )
+}
